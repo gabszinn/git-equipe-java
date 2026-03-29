9ecb8838db2f59b7043513e81d3f67fba19591a5
Gabriel Felipe <you@example.com>
2026-03-29 19:55:30 -0300

    adicionado subclasse Gato

---
+++ gitequipe/src/main/java/git/equipe/modelo/Gato.java
@@ -0,0 +1,29 @@
+package git.equipe.modelo;
+
+public class Gato extends Animal {
+
+    private String raca;
+
+    public Cachorro(String especie, String sexo, int idade, String raca) {
+        super(especie, sexo, idade);
+        this.raca = raca;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    @Override
+    public String emitirSom() {
+        return "Au au!";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Raça: " + raca;
+    }
+}
