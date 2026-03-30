c853e6c982ea8a903aada436ea1dcec9d7e763ed
gabszinn <agabs506@gmail.com>
2026-03-29 22:22:25 -0300

    atributo unico "cor" adicionado a subclasse

--- gitequipe/src/main/java/git/equipe/modelo/Gato.java
+++ gitequipe/src/main/java/git/equipe/modelo/Gato.java
@@ -2,8 +2,19 @@ package git.equipe.modelo;
 
 public class Gato extends Animal {
 
-    public Gato(String especie, String sexo, int idade) {
+    private String cor;
+
+    public Gato(String especie, String sexo, int idade, String cor) {
         super(especie, sexo, idade);
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
     }
 
     @Override
@@ -11,8 +22,11 @@ public class Gato extends Animal {
         return "Miau Miau!";
     }
 
-    @Override
-    public String toString() {
-        return "Especie:  " + especie + ", Idade:  " + idade + " Sexo: " + sexo;
-    }
-}
+@Override
+public String toString() {
+    return "Especie: " + especie +
+           ", Sexo: " + sexo +
+           ", Idade: " + idade +
+           ", Cor: " + cor;
+  }
+}  
